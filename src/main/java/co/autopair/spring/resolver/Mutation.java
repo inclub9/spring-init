86c1bb62e03f45ff4b685b50f4270dcf3e64d9e2
supphanut kala <inclub9@gmail.com>
2020-06-22 10:48:40 +0700

    graphql implement team DTO

--- src/main/java/co/autopair/spring/resolver/Mutation.java
+++ src/main/java/co/autopair/spring/resolver/Mutation.java
@@ -1,71 +1,20 @@
-//package co.autopair.spring.resolver;
-//
-//import co.autopair.spring.entity.Author;
-//import co.autopair.spring.entity.Tutorial;
-//import co.autopair.spring.repository.AuthorRepository;
-//import co.autopair.spring.repository.TutorialRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Component;
-//
-//import com.coxautodev.graphql.tools.GraphQLMutationResolver;
-//
-//import javassist.NotFoundException;
-//
-//import java.util.Optional;
-//
-//@Component
-//public class Mutation implements GraphQLMutationResolver {
-//    private AuthorRepository authorRepository;
-//    private TutorialRepository tutorialRepository;
-//
-//    @Autowired
-//    public Mutation(AuthorRepository authorRepository, TutorialRepository tutorialRepository) {
-//        this.authorRepository = authorRepository;
-//        this.tutorialRepository = tutorialRepository;
-//    }
-//
-//    public Author createAuthor(String name, Integer age) {
-//        Author author = new Author();
-//        author.setName(name);
-//        author.setAge(age);
-//
-//        authorRepository.save(author);
-//
-//        return author;
-//    }
-//
-//    public Tutorial createTutorial(String title, String description, Long authorId) {
-//        Tutorial tutorial = new Tutorial();
-//        tutorial.setAuthor(new Author(authorId));
-//        tutorial.setTitle(title);
-//        tutorial.setDescription(description);
-//
-//        tutorialRepository.save(tutorial);
-//
-//        return tutorial;
-//    }
-//
-//    public boolean deleteTutorial(Long id) {
-//        tutorialRepository.deleteById(id);
-//        return true;
-//    }
-//
-//    public Tutorial updateTutorial(Long id, String title, String description) throws NotFoundException {
-//        Optional<Tutorial> optTutorial = tutorialRepository.findById(id);
-//
-//        if (optTutorial.isPresent()) {
-//            Tutorial tutorial = optTutorial.get();
-//
-//            if (title != null)
-//                tutorial.setTitle(title);
-//            if (description != null)
-//                tutorial.setDescription(description);
-//
-//            tutorialRepository.save(tutorial);
-//            return tutorial;
-//        }
-//
-//        throw new NotFoundException("Not found Tutorial to update!");
-//    }
-//
-//}
+package co.autopair.spring.resolver;
+
+import co.autopair.spring.entity.Team;
+import co.autopair.spring.repository.TeamRepository;
+import com.coxautodev.graphql.tools.GraphQLMutationResolver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Mutation implements GraphQLMutationResolver {
+
+    @Autowired
+    private TeamRepository teamRepository;
+
+    public Team createTeam(String name) {
+        return teamRepository.save(
+                Team.builder().name(name).build()
+        );
+    }
+}
