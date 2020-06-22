5ad6caf44e26c2fec55cd5ce0e1e43c87302952a
supphanut kala <inclub9@gmail.com>
2020-06-22 14:49:39 +0700

    remove unused code

--- src/main/java/co/autopair/spring/repository/AuthorRepository.java
+++
@@ -1,9 +0,0 @@
-//package co.autopair.spring.repository;
-//
-//import co.autopair.spring.entity.Author;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//
-//public interface AuthorRepository extends JpaRepository<Author, Long> {
-//
-//}
--- src/main/java/co/autopair/spring/repository/TutorialRepository.java
+++
@@ -1,9 +0,0 @@
-//package co.autopair.spring.repository;
-//
-//import co.autopair.spring.entity.Tutorial;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//
-//public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
-//
-//}
